0f32c063464258597cf72429892cf83783275efd
bogdi xameli <121572305+gmsugff@users.noreply.github.com>
2025-06-23 13:39:35 +0300

    1

---
+++ p32bx datebasse_2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace p32bx_datebasse_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
